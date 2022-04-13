22f1914c87553038ea03f9665da43fceea0de06a
Strong Recomme <werasik2aa@yandex.ru>
2022-04-13 20:16:41 +0700

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+[assembly: AssemblyTitle("vehiclemod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("vehiclemod")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(vehiclemod.main), "VehicleMod", "1.09", "NopirateOnlySteam")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b1e85fa5-23d9-48b6-9d96-e9afe7c41d28")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
